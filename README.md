8336e81079ad5297ca8f8ea3ced17889b52fa9dd
osvaldoaurelio <osvaldo.aurelio@outlook.com>
2020-04-04 20:05:35 -0300

    front end finished

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# semanaomnistack11
+<h1>semanaomnistack11</h1>
+
+[Google](https://www.google.com.br){:target="_blank_"}
+
 Projeto feito na 11ª Semana Omnistack da RocketSeat
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+// import Header from './Header'
+
+import "./global.css";
+import Routes from './routes' // import Logon from "./pages/logon";
 
 function App() {
+  // const [ counter, setCounter ] = useState(0) // array[valorDaVariavel, funcaoDeAtualizacaoDoValor]
+
+  // function increment() {
+  //   setCounter(counter + 1)
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes />
+
+    // <Header>
+    //   Contador: {counter}
+    //   <button onClick={increment}>incrementar</button>
+    // </Header>
   );
 }
 
